b898f0bfef19340d5594109ec3d04933b8a24194
Gabriel Brinck da Silva <s33233@pjwstk.edu.pl>
2026-03-16 11:28:57 +0100

    Added userName handling

--- APBD_Tut2/APBD_Tut2/Program.cs
+++ APBD_Tut2/APBD_Tut2/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Input your name");
+String userName = Console.ReadLine();
+Console.WriteLine("Hello "+userName+"!");
