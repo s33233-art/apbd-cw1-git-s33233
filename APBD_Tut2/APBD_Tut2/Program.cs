c7c4ac5b62072dda5467041b7e9c5f5ec6e91386
Gabriel Brinck da Silva <s33233@pjwstk.edu.pl>
2026-03-16 11:32:43 +0100

    Added User Number Input

--- APBD_Tut2/APBD_Tut2/Program.cs
+++ APBD_Tut2/APBD_Tut2/Program.cs
@@ -3,3 +3,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Input your name");
 String userName = Console.ReadLine();
 Console.WriteLine("Hello "+userName+"!");
+Console.WriteLine("Choose a number");
+int userNum = Convert.ToInt32(Console.ReadLine());
